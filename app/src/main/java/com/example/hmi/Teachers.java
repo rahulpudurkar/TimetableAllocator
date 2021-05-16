9a09325b1c81cf7e6a3b9c053e2b80dd4b61dd9a
Rahul Pudurkar <rahulpudurkar68@gmail.com>
2021-05-16 22:16:29 +0530

    Commit

--- app/src/main/java/com/example/hmi/Teachers.java
+++ app/src/main/java/com/example/hmi/Teachers.java
@@ -8,17 +8,65 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Teachers extends AppCompatActivity {
     private Button back;
+    TextView t1,t2,t3,t4,t5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teachers);
         back = (Button)findViewById(R.id.button_back);
+        t1=findViewById(R.id.FE);
+        t2=findViewById(R.id.SE);
+        t3=findViewById(R.id.TE);
+        t4=findViewById(R.id.BE);
+        t5=findViewById(R.id.GE);
+        t1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Questions.class));
+                overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
+
+            }
+        });
+        t2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Questions.class));
+                overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
+
+            }
+        });
+        t3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Questions.class));
+                overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
+
+            }
+        });
+        t4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Questions.class));
+                overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
+
+            }
+        });
+        t5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Questions.class));
+                overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
+
+            }
+        });
+
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setSelectedItemId(R.id.dashboard);
