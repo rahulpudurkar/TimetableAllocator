2e54f6361e5ba94681268df0421cc97b687cab90
Rahul Pudurkar <rahulpudurkar68@gmail.com>
2021-05-17 17:14:41 +0530

    Commit

--- app/src/main/java/com/example/hmi/facultyinformation.java
+++ app/src/main/java/com/example/hmi/facultyinformation.java
@@ -2,23 +2,129 @@ package com.example.hmi;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class facultyinformation extends AppCompatActivity {
-    private Button back;
+    private Button back,whatsapp;
+    TextView t1,t2,t3,t4,t5,t6,t7,p1,p2,p3,p4,p5,p6,p7;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_facultyinformation);
         back = (Button)findViewById(R.id.button_back);
+        whatsapp=(Button)findViewById(R.id.whatsapp);
+        t1= (TextView)findViewById(R.id.t1);
+        t2= (TextView)findViewById(R.id.t2);
+        t3= (TextView)findViewById(R.id.t3);
+        t4= (TextView)findViewById(R.id.t4);
+        t5= (TextView)findViewById(R.id.t5);
+        t6= (TextView)findViewById(R.id.t6);
+        t7= (TextView)findViewById(R.id.t7);
+        p1= (TextView)findViewById(R.id.p1);
+        p2= (TextView)findViewById(R.id.p2);
+        p3= (TextView)findViewById(R.id.p3);
+        p4= (TextView)findViewById(R.id.p4);
+        p5= (TextView)findViewById(R.id.p5);
+        p6= (TextView)findViewById(R.id.p6);
+        p7= (TextView)findViewById(R.id.p7);
+
+
+        p1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:9223370153"));
+                startActivity(intent);
+            }
+        });
+
+        p2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:9975321741"));
+                startActivity(intent);
+            }
+        });
+
+        p3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:9970284471"));
+                startActivity(intent);
+            }
+        });
+
+        p4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:9594198786"));
+                startActivity(intent);
+            }
+        });
+
+        p5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:9833029544"));
+                startActivity(intent);
+            }
+        });
+
+        p6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:9892524834"));
+                startActivity(intent);
+            }
+        });
+
+        p7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:9820955452"));
+                startActivity(intent);
+            }
+        });
+
+
+        whatsapp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            /*    Intent intent = getPackageManager().getLaunchIntentForPackage("com.whatsapp");
+                startActivity(intent);*/
+
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.setPackage("com.whatsapp");
+                sendIntent.putExtra(Intent.EXTRA_TEXT, "Hello Sir/Madam I need to Swap my Lecture with you today. Please Revert back asap.");
+                sendIntent.setType("text/plain");
+                startActivity(sendIntent);
+
+
+            }
+        });
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
